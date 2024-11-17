eea53a5a0f611bce452d08a244088a416082bc4c
Chey DB <cheydb@rocketmail.com>
2024-11-17 11:29:10 +0000

    AI Aseembly Real time voice recognition implemented

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,10 +1,11 @@
 import os
 import requests
 import json
+import assemblyai as aai
 from typing import List
 from pydantic import BaseModel
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
+from elevenlabs import generate, stream
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from groq import Groq
@@ -15,6 +16,7 @@ load_dotenv()
 # Environment variables
 XI_API_KEY = os.getenv("XI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+AAI_API_KEY = os.getenv("AAI_API_KEY")
 
 # Selenium setup to scrape the job description
 url = "https://www.metacareers.com/v2/jobs/1303076114202551/"
@@ -70,100 +72,100 @@ interview_questions = get_interview_questions(job_description)
 print(json.dumps(interview_questions.model_dump(), indent=2))
 
 
-# class AI_Assistant:
-#     def __init__(self):
-#         aai.settings.api_key = "ASSEMBLYAI-API-KEY"
-#         self.openai_client = OpenAI(api_key="OPENAI-API-KEY")
-#         self.elevenlabs_api_key = "ELEVENLABS-API-KEY"
-#
-#         self.transcriber = None
-#
-#         # Prompt
-#         self.full_transcript = [
-#             {"role": "system", "content": "You are a receptionist at a dental clinic. Be resourceful and efficient."},
-#         ]
-#
-#     ###### Step 2: Real-Time Transcription with AssemblyAI ######
-#
-#     def start_transcription(self):
-#         self.transcriber = aai.RealtimeTranscriber(
-#             sample_rate=16000,
-#             on_data=self.on_data,
-#             on_error=self.on_error,
-#             on_open=self.on_open,
-#             on_close=self.on_close,
-#             end_utterance_silence_threshold=1000
-#         )
-#
-#         self.transcriber.connect()
-#         microphone_stream = aai.extras.MicrophoneStream(sample_rate=16000)
-#         self.transcriber.stream(microphone_stream)
-#
-#     def stop_transcription(self):
-#         if self.transcriber:
-#             self.transcriber.close()
-#             self.transcriber = None
-#
-#     def on_open(self, session_opened: aai.RealtimeSessionOpened):
-#         print("Session ID:", session_opened.session_id)
-#         return
-#
-#     def on_data(self, transcript: aai.RealtimeTranscript):
-#         if not transcript.text:
-#             return
-#
-#         if isinstance(transcript, aai.RealtimeFinalTranscript):
-#             self.generate_ai_response(transcript)
-#         else:
-#             print(transcript.text, end="\r")
-#
-#     def on_error(self, error: aai.RealtimeError):
-#         print("An error occured:", error)
-#         return
-#
-#     def on_close(self):
-#         # print("Closing Session")
-#         return
-#
-#     ###### Step 3: Pass real-time transcript to OpenAI ######
-#
-#     def generate_ai_response(self, transcript):
-#
-#         self.stop_transcription()
-#
-#         self.full_transcript.append({"role": "user", "content": transcript.text})
-#         print(f"\nPatient: {transcript.text}", end="\r\n")
-#
-#         response = self.openai_client.chat.completions.create(
-#             model="gpt-3.5-turbo",
-#             messages=self.full_transcript
-#         )
-#
-#         ai_response = response.choices[0].message.content
-#
-#         self.generate_audio(ai_response)
-#
-#         self.start_transcription()
-#         print(f"\nReal-time transcription: ", end="\r\n")
-#
-#     ###### Step 4: Generate audio with ElevenLabs ######
-#
-#     def generate_audio(self, text):
-#
-#         self.full_transcript.append({"role": "assistant", "content": text})
-#         print(f"\nAI Receptionist: {text}")
-#
-#         audio_stream = generate(
-#             api_key=self.elevenlabs_api_key,
-#             text=text,
-#             voice="Rachel",
-#             stream=True
-#         )
-#
-#         stream(audio_stream)
-#
-#
-# greeting = "Thank you for calling Vancouver dental clinic. My name is Sandy, how may I assist you?"
-# ai_assistant = AI_Assistant()
-# ai_assistant.generate_audio(greeting)
-# ai_assistant.start_transcription()
+class AI_Assistant:
+    def __init__(self):
+        aai.settings.api_key = AAI_API_KEY
+        self.groq_client = groq = Groq(api_key=GROQ_API_KEY)
+        self.elevenlabs_api_key = XI_API_KEY
+
+        self.transcriber = None
+
+        # Prompt
+        self.full_transcript = [
+            {"role": "system", "content": "You are an expert interviewer for job roles in the tech industry. You ask good follow up questions to make sure the candidate is the right fit for the role."},
+        ]
+
+    ###### Step 2: Real-Time Transcription with AssemblyAI ######
+
+    def start_transcription(self):
+        self.transcriber = aai.RealtimeTranscriber(
+            sample_rate=16000,
+            on_data=self.on_data,
+            on_error=self.on_error,
+            on_open=self.on_open,
+            on_close=self.on_close,
+            end_utterance_silence_threshold=1000
+        )
+
+        self.transcriber.connect()
+        microphone_stream = aai.extras.MicrophoneStream(sample_rate=16000)
+        self.transcriber.stream(microphone_stream)
+
+    def stop_transcription(self):
+        if self.transcriber:
+            self.transcriber.close()
+            self.transcriber = None
+
+    def on_open(self, session_opened: aai.RealtimeSessionOpened):
+        print("Session ID:", session_opened.session_id)
+        return
+
+    def on_data(self, transcript: aai.RealtimeTranscript):
+        if not transcript.text:
+            return
+
+        if isinstance(transcript, aai.RealtimeFinalTranscript):
+            self.generate_ai_response(transcript)
+        else:
+            print(transcript.text, end="\r")
+
+    def on_error(self, error: aai.RealtimeError):
+        print("An error occured:", error)
+        return
+
+    def on_close(self):
+        # print("Closing Session")
+        return
+
+    ###### Step 3: Pass real-time transcript to OpenAI ######
+
+    def generate_ai_response(self, transcript):
+
+        self.stop_transcription()
+
+        self.full_transcript.append({"role": "user", "content": transcript.text})
+        print(f"\nInterviewee: {transcript.text}", end="\r\n")
+
+        response = self.groq_client.chat.completions.create(
+            model="llama3-groq-70b-8192-tool-use-preview",
+            messages=self.full_transcript
+        )
+
+        ai_response = response.choices[0].message.content
+
+        self.generate_audio(ai_response)
+
+        self.start_transcription()
+        print(f"\nReal-time transcription: ", end="\r\n")
+
+    ###### Step 4: Generate audio with ElevenLabs ######
+
+    def generate_audio(self, text):
+
+        self.full_transcript.append({"role": "assistant", "content": text})
+        print(f"\nAI Receptionist: {text}")
+
+        audio_stream = generate(
+            api_key=self.elevenlabs_api_key,
+            text=text,
+            voice="Alice",
+            stream=True
+        )
+
+        stream(audio_stream)
+
+
+greeting = "Hi Chey, how are you today?"
+ai_assistant = AI_Assistant()
+ai_assistant.generate_audio(greeting)
+ai_assistant.start_transcription()
